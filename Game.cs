9aca0b5658520fa8f4244c69a01e14e6324b499f
Thane <thabay0606@students.maquoketaschools.org>
2026-05-22 12:20:57 -0500

    S4

--- Game.cs
+++ Game.cs
@@ -1,41 +1,70 @@
+using System.Text;
+using System.Linq;
+
 class Game{
     // Make your changes in this file
 
+    private string phrase;
+
     public Game(string phrase){
-        this.phrase = phrase;
+        this.phrase = phrase.ToUpper();
     }
 
     public string DisplayBlanks(){
-        string result = " ";
+        StringBuilder blanks = new StringBuilder();
 
         foreach (char c in phrase){
             if (c == ' '){
-                result += " ";
+                blanks.Append(" ");
             } else {
-                result += "_";
+                blanks.Append("_");
             }
         }
 
-        return result.TrimEnd();
+        return blanks.ToString().TrimEnd();
 
         
     }
 
     public string Play(char[] guessedLetters){
 
+        string guessed = new string(guessedLetters).ToUpper();
 
+        if (!IsValid(guessed)){
+            return DisplayBlanks();
+        }
 
+        StringBuilder result = new StringBuilder();
 
-        return ;
+        foreach (char c in phrase){
+            if (c == ' '){
+                result.Append(c + " ");
+            }
+            else if (guessed.Contains(c)){
+                result.Append(c + " ");
+            }
+            else{
+                result.Append("_");
+            }
+        }
+
+        return result.ToString().TrimEnd();
     }
         
     public bool IsValid(string guessedLetters){
-        int lettersAsInt = guessedLetters.Length;
+        if (guessedLetters.Length != 10){
+            return false;
+        }
 
-        if(lettersAsInt > 10 || lettersAsInt < 10 || guessedLetters.Contains(" ")){
-            return false; 
-        } 
-            return true; 
+        if (guessedLetters.Contains(" ")){
+            return false;
+        }
+
+        if (!guessedLetters.All(char.IsLetter)){
+            return false;
+        }
+
+        return true;
 
     }
 }
